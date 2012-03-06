2f1d92defd5ca551710498e7b6846ec16c4fee02
derikg1 <derikg1@yahoo.com>
2012-03-06 01:59:16 +0000

    Assuming this is a the menu screen, all of these will correspond to a a page listed under the src folder.

--- MealTime/src/edu/iastate/mis438/mealtime/startingPoint.java
+++ MealTime/src/edu/iastate/mis438/mealtime/startingPoint.java
@@ -2,12 +2,36 @@ package edu.iastate.mis438.mealtime;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+//I edited this directly from google code, not from eclipse. <-- Derik
+// Did we want a list view for this first prototype or actual buttons? 
+//***This is to create a listview on the home screen. We would still need
+//to change the items in the android manafest and main layout.***
 public class startingPoint extends Activity {
+  String classes[] = {"StartingPoint", "Search", "Login", "Recipes", "Food History", "Weight Tracker"};
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		//which list item was clicked
+		String stringPosition = classes[position];
+		
+		try{
+		//this tries to load up the activity that was clicked from the main menu. StartingPoint is the homescreen I'm assuming? 
+      //We still need a splash screen.
+		Class ourClass = Class.forName("edu.iastate.mis438.mealtime." + stringPosition);
+		Intent ourIntent = new Intent(startingPoint.this, ourClass);
+		startActivity(ourIntent);
+		
+		} 
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
 }
